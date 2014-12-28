0bf83d982b356b36b9b6d263d4b8f48ebe9d108e
itCherokee <simeonov23@yahoo.com>
2014-12-28 11:56:08 +0200

    Added support for angular ui router

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,38 @@
 'use strict';
 
-// Declare app level module which depends on views, and components
-angular.module('adsSystem', ['ui.bootstrap']).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+angular.module('adsSystem', ['ui.bootstrap', 'ui.router']).
+    config(function ($stateProvider, $urlRouterProvider) {
+        //$stateProvider and $urlRouterProvider are from ui.router module
+        $stateProvider
+            .state('allAds', {
+                url: '/ads',
+                controller: 'Controller1',
+                templateUrl: '/ads/allads.html'
+            })
+            .state('login', {
+                url: '/login',
+                controller: 'LoginController as login',
+                templateUrl: '/login/views/login.html'
+            })
+            .state('register', {
+                url: '/login',
+                controller: 'RegisterController as register',
+                templateUrl: '/login/views/register.html'
+            })
+            .state('user', {
+                url: '/user',
+                controller: 'UserController as user',
+                templateUrl: '/user/views/user.html'
+            })
+//            .state('view2', {
+//                url: '/view2/:firstname/:lastname',
+//                controller: 'Controller2',
+//                resolve: {
+//                    names: function () {
+//                        return;
+//                    }
+//                },
+//                templateUrl: '/partials/view2.html'
+//            });
+        $urlRouterProvider.otherwise('/view1');
+    });
