b82c9018e3d1f18302ff8f325d63b84c377fca6a
itCherokee <simeonov23@yahoo.com>
2014-12-29 16:20:15 +0200

    added asynchronous loading of libs and dependency loader

--- app/js/app.js
+++ app/js/app.js
@@ -1,43 +1,8 @@
 'use strict';
 
-angular.module('adsSystem', ['ui.bootstrap', 'ui.router']).
-    config(function ($stateProvider, $urlRouterProvider) {
-        //$stateProvider and $urlRouterProvider are from ui.router module
-        $stateProvider
-            .state('home', {
-                url: '/',
-                controller: 'HomeController',
-                templateUrl: '/index.html'
-            })
-            .state('login', {
-                url: '/login',
-                controller: 'LoginController as login',
-                templateUrl: '/login/views/login.html'
-            })
-            .state('register', {
-                url: '/register',
-                controller: 'RegisterController as register',
-                templateUrl: '/login/views/register.html'
-            });
-//            .state('user', {
-//                url: '/user',
-//                controller: 'UserController as user',
-//                templateUrl: '/user/views/user.html'
-//            });
-//            .state('view2', {
-//                url: '/view2/:firstname/:lastname',
-//                controller: 'Controller2',
-//                resolve: {
-//                    names: function () {
-//                        return;
-//                    }
-//                },
-//                templateUrl: '/partials/view2.html'
-//            });
-        $urlRouterProvider.otherwise('/');
-    })
+angular.module('adsSystem', ['ui.bootstrap', 'ui.router', 'adsSystem.ads'])
     .run(['$state', '$rootScope', '$stateParams', function ($state, $rootScope, $stateParams) {
         $rootScope.$state = $state;
         $rootScope.$stateParams = $stateParams;
-        $state.on('/')
+        $state.on('home');
     }]);
---
+++ app/modules/ads/js/adsModule.js
@@ -0,0 +1,37 @@
+'use strict';
+
+angular.module('adsSystem.ads', []).
+    config(function ($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state('home', {
+                url: '/',
+                controller: 'HomeController',
+                templateUrl: '/views/home.html'
+            })
+            .state('login', {
+                url: '/login',
+                controller: 'LoginController as login',
+                templateUrl: '/login/views/login.html'
+            })
+            .state('register', {
+                url: '/register',
+                controller: 'RegisterController as register',
+                templateUrl: '/login/views/register.html'
+            });
+//            .state('user', {
+//                url: '/user',
+//                controller: 'UserController as user',
+//                templateUrl: '/user/views/user.html'
+//            });
+//            .state('view2', {
+//                url: '/view2/:firstname/:lastname',
+//                controller: 'Controller2',
+//                resolve: {
+//                    names: function () {
+//                        return;
+//                    }
+//                },
+//                templateUrl: '/partials/view2.html'
+//            });
+        $urlRouterProvider.otherwise('/');
+    })
