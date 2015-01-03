f7121d6ebcb77660f43e1f11a593d940f8790a46
itCherokee <simeonov23@yahoo.com>
2015-01-03 18:40:26 +0200

    register form ng-model applied

---
+++ app/modules/ads/js/controllers/homeController.js
@@ -0,0 +1,76 @@
+'use strict';
+
+angular.module('adsSystem').controller('HomeController', ['dalService', '$scope', '$log', function (dalService, $scope, $log) {
+    $scope.townId = '';
+    $scope.categoryId = '';
+    $scope.startPage = 1;
+
+    var allElement = {
+        'id': '',
+        'name': 'All'
+    };
+
+    function loadAds() {
+        dalService.getAllPublishedAds($scope.startPage, $scope.townId, $scope.categoryId)
+            .success(function (data) {
+                $scope.ads = data.ads;
+
+            })
+            .error(function (error) {
+                $log.error('Ads can not be loaded from server!');
+            });
+    }
+
+    dalService.getAllCategories()
+        .success(function (data) {
+            var categories = {
+                selected: null,
+                data: data
+            };
+
+            categories.data.unshift(allElement);
+            categories.selected = categories.data[0];
+            $scope.categories = categories;
+        })
+        .error(function (error) {
+            $log.error('Categories cannot be loaded from server!');
+        });
+
+    dalService.getAllTowns()
+        .success(function (data) {
+            var towns = {
+                selected: null,
+                data: data
+            };
+
+            towns.data.unshift(allElement);
+            towns.selected = towns.data[0];
+            $scope.towns = towns;
+        })
+        .error(function (error) {
+            $log.error('Towns cannot be loaded from server!');
+        });
+
+    $scope.clickCategoryHandler = function clickCategoryHandler(categoryId) {
+        $scope.categoryId = categoryId;
+        $scope.isCategoryFilterStrict = categoryId !== '';
+//        $scope.startPage = 1;
+//        loadAds();
+    };
+
+    $scope.clickTownHandler = function clickTownHandler(townId) {
+        $scope.townId = townId;
+        $scope.isTownFilterStrict = townId !== '';
+//        $scope.startPage = 1;
+//
+//        loadAds();
+    };
+
+    $scope.accordionStatus = {
+        showOneItem: true,
+        categoryIsOpen: false,
+        townIsOpen: false
+    };
+
+    loadAds();
+}]);
