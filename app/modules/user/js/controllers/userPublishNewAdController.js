cb8edad9f028e700f85970451425c8bc2edd1633
itCherokee <simeonov23@yahoo.com>
2015-01-09 14:59:20 +0200

    using ng-flow in Create NEw Ad

--- app/modules/user/js/controllers/userPublishNewAdController.js
+++ app/modules/user/js/controllers/userPublishNewAdController.js
@@ -1,8 +1,9 @@
 'use strict';
 
 angular.module('adsSystem.user')
-    .controller('UserPublishNewAdController', ['$rootScope', '$scope', 'userService', 'townService', 'categoryService', '$state',
-        function ($rootScope, $scope, userService, townService, categoryService, $state) {
+    .controller('UserPublishNewAdController', ['$rootScope', '$scope', 'userService', 'townService', 'categoryService',
+            '$state', 'infoService',
+        function ($rootScope, $scope, userService, townService, categoryService, $state, infoService) {
             $rootScope.$broadcast("PageChanged", 'Publish New Ad');
 
             var newAd = {
@@ -47,18 +48,18 @@ angular.module('adsSystem.user')
                     });
             };
 
-            $scope.fileSelected = function (fileInputField) {
+            $scope.fileSelected = function ($file) {
                 delete $scope.ad.imageDataUrl;
-                var file = fileInputField.files[0];
+                var file = $file.file;
                 if (file.type.match(/image\/.*/)) {
                     var reader = new FileReader();
                     reader.onload = function () {
                         $scope.ad.imageDataUrl = reader.result;
-                        $(".image-box img").attr("src", reader.result);
+//                        $(".image-box img").attr("src", reader.result);
                     };
                     reader.readAsDataURL(file);
                 } else {
-                    $(".image-box").html("<p>File type not supported!</p>");
+                    infoService.warning('File type not supported!');
                 }
             };
 
