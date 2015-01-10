d5da1faee92ee29487bad504a39792a459aa9842
itCherokee <simeonov23@yahoo.com>
2015-01-10 20:03:33 +0200

    image change done in Edit Ad view

--- app/modules/user/js/controllers/userMyAdsEditController.js
+++ app/modules/user/js/controllers/userMyAdsEditController.js
@@ -4,7 +4,7 @@ angular.module('adsSystem.user')
     .controller('UserMyAdsEditController',
     ['$rootScope', '$scope', '$state', 'userService', '$stateParams', 'townService', 'categoryService', 'infoService', 'flowFactory',
         function ($rootScope, $scope, $state, userService, $stateParams, townService, categoryService, infoService, flowFactory) {
-            $rootScope.$broadcast("PageChanged", 'Delete Ad');
+            $rootScope.$broadcast("PageChanged", 'Edit Ad');
 
             $scope.image = {};
 
@@ -32,50 +32,60 @@ angular.module('adsSystem.user')
             userService.getUserAdById($stateParams.id)
                 .success(function (data) {
                     $scope.ad = data;
-                    if (data.imageDataUrl) {
-                        var blob = new Blob([data.imageDataUrl], {type: "image/png"});
-                        blob.name = 'file.png';
-
-                     //   $scope.image.flow.files = [blob];
-                    }
+//                    if (data.imageDataUrl) {
+//                        var blob = new Blob([data.imageDataUrl], {type: "image/png"});
+//                        blob.name = 'file.png';
+//                        $scope.image.flow.pause();
+//                        $scope.image.flow.addFile(blob);
+//                    }
                 })
                 .error(function (error) {
                     infoService.error('Error fetching from server the requested Ad for editing.');
                 });
 
-            $scope.fileSelected = function (fileInputField) {
+            $scope.convertImage = function ($file) {
                 delete $scope.ad.imageDataUrl;
-                var file = fileInputField.files[0];
+                var file = $file.file;
                 if (file.type.match(/image\/.*/)) {
                     var reader = new FileReader();
                     reader.onload = function () {
                         $scope.ad.imageDataUrl = reader.result;
-//                        $(".image-box").html("<img src='" + reader.result + "'>");
                     };
                     reader.readAsDataURL(file);
                 } else {
                     infoService.warning('File type not supported!');
                 }
-            };
+            }
 
             // Event handler for editing selected Ad
             $scope.editAd = function () {
-                userService.editUserAd($scope.ad, $scope.imageStatus ? $scope.imageStatus : undefined)
-                    .success(function (data) {
-                        $rootScope.$broadcast('userAdEdited');
-                        infoService.success('Advertisement successfully edited.');
-                        $state.go('userMyAds');
-                    })
-                    .error(function (error) {
-                        infoService.error('An error occurred. Advertisement changes has not been saved.');
-                    });
+                if ($scope.form.$valid) {
+                    if ($scope.image.flow.files.length) {
+                        $scope.imageStatus = 'update';
+//                        convertImage($scope.image.flow.files[0]);
+                    }
+
+                    userService.editUserAd($scope.ad, $scope.imageStatus ? $scope.imageStatus : undefined)
+                        .success(function (data) {
+                            $rootScope.$broadcast('userAdEdited');
+                            infoService.success('Advertisement successfully edited.');
+                            $state.go('userMyAds');
+                        })
+                        .error(function (error) {
+                            infoService.error('An error occurred. Advertisement changes has not been saved.');
+                        });
+                } else {
+                    infoService.error('All fields marked in red are mandatory!');
+                }
             };
 
             $scope.deleteImage = function () {
                 $scope.imageStatus = 'delete';
+                $scope.image.flow.cancel();
+                delete $scope.ad.imageDataUrl;
             };
 
-            $scope.changeImage = function () {
-                $scope.imageStatus = 'update';
-            }
+//            $scope.changeImage = function () {
+//
+//            }
         }]);
