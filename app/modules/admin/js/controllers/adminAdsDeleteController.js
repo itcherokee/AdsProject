6164cd76b6592799c2f1f32cad511abb5cdd84d6
itCherokee <simeonov23@yahoo.com>
2015-01-11 12:38:01 +0200

    admin delete ad controller

---
+++ app/modules/admin/js/controllers/adminAdsDeleteController.js
@@ -0,0 +1,30 @@
+'use strict';
+
+angular.module('adsSystem.user')
+    .controller('AdminAdsDeleteController', ['$rootScope', '$scope', '$state', 'adminService', '$stateParams', 'infoService',
+        function ($rootScope, $scope, $state, adminService, $stateParams, infoService) {
+            $rootScope.$broadcast("PageChanged", 'Ads Administration - Delete Ad');
+
+            adminService.getAdById($stateParams.id)
+                .success(function (data) {
+                    $scope.ad = data;
+                })
+                .error(function (error) {
+                    infoService.error('Unable to get data from server - requested Advertisement.')
+                });
+
+            // Event handler for deleting selected Ad
+            $scope.deleteAd = function () {
+                adminService.deleteUserAdById($scope.ad.id)
+                    .success(function (data) {
+                        $rootScope.$broadcast('userAdDeleted');
+                        infoService.success('Advertisement has been deleted successfully.');
+
+                        $state.go('userMyAds');
+                    })
+                    .error(function (error) {
+                        infoService.error('Error occurred. Advertisement cannot be deleted.')
+                    });
+
+            };
+        }]);
